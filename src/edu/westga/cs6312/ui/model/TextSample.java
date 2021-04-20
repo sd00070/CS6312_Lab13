3505dc20678b73e421df78296aff39a0744ae9e0
Spencer Dent <sd00070@my.westga.edu>
2021-04-20 02:53:23 -0400

    throw errors on invalid arguments

--- src/edu/westga/cs6312/ui/model/TextSample.java
+++ src/edu/westga/cs6312/ui/model/TextSample.java
@@ -18,8 +18,22 @@ public class TextSample {
 	 * @param newMessage        - the message to display
 	 * @param newFontFamilyName - the name of the font family to use
 	 * @param newFontSize       - the font size to use
+	 * @precondition newMessage != null && newFontFamilyName != null && newFontSize
+	 *               > 0.0
+	 * @throws IllegalArgumentException if newMessage == null || newFontFamily ==
+	 *                                  null || newFontSize <= 0.0
 	 */
 	public TextSample(String newMessage, String newFontFamilyName, double newFontSize) {
+		if (newMessage == null) {
+			throw new IllegalArgumentException("Invalid message, message cannot be null.");
+		}
+		if (newFontFamilyName == null) {
+			throw new IllegalArgumentException("Invalid font family name, font family name cannot be null.");
+		}
+		if (newFontSize <= 0.0) {
+			throw new IllegalArgumentException("Invalid font size, font size must be greater than zero (0.0).");
+		}
+
 		this.message = newMessage;
 		this.fontFamilyName = newFontFamilyName;
 		this.fontSize = newFontSize;
@@ -38,8 +52,14 @@ public class TextSample {
 	 * Changes the message to the provided String.
 	 * 
 	 * @param newMessage - the String to set as the message
+	 * @precondition newMessage != null
+	 * @throws IllegalArgumentException if newMessage == null
 	 */
 	public void setMessage(String newMessage) {
+		if (newMessage == null) {
+			throw new IllegalArgumentException("Invalid message, message cannot be null.");
+		}
+
 		this.message = newMessage;
 	}
 
@@ -56,8 +76,14 @@ public class TextSample {
 	 * Changes the font family name to the provided String.
 	 * 
 	 * @param newFontFamilyName - the name of the new font family
+	 * @precondition newFontFamilyName != null
+	 * @throws IllegalArgumentException if newFontFamilyName == null
 	 */
 	public void setFontFamilyName(String newFontFamilyName) {
+		if (newFontFamilyName == null) {
+			throw new IllegalArgumentException("Invalid font family name, font family name cannot be null.");
+		}
+
 		this.fontFamilyName = newFontFamilyName;
 	}
 
@@ -74,8 +100,13 @@ public class TextSample {
 	 * Changes the font size to the given value.
 	 * 
 	 * @param newFontSize - the new value to set the font size to
+	 * @precondition newFontSize > 0.0
+	 * @throws IllegalArgumentException if newFontSize <= 0.0
 	 */
 	public void setFontSize(double newFontSize) {
+		if (newFontSize <= 0.0) {
+			throw new IllegalArgumentException("Invalid font size, font size must be greater than zero (0.0).");
+		}
 		this.fontSize = newFontSize;
 	}
 
